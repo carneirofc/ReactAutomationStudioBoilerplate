6dc60fe9ca1fec13a01ee90666db661494859873
carneirofc <claudiofcarneiro@hotmail.com>
2020-03-17 13:02:39 -0300

    Main.js format

--- ReactApp/src/Main.js
+++ ReactApp/src/Main.js
@@ -1,103 +1,82 @@
 import React, { Component } from 'react';
 
-import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { withRouter } from 'react-router-dom';
 
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Button from '@material-ui/core/Button';
 import RedirectToLogIn from './React-Automation-Studio/components/SystemComponents/RedirectToLogin.js';
 import SideBar from './React-Automation-Studio/components/SystemComponents/SideBar';
 import Grid from '@material-ui/core/Grid';
-import { Redirect } from 'react-router-dom'
 // Styles
-
-
-
-
 const styles = theme => ({
   root: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
-
-
-
   },
   center: {
     margin: 'auto',
-
     width: '15%',
-    height:'50%'
-
+    height: '50%'
   },
   button: {
-    width:'100%',
-    height:'100%',
-    marginTop:'auto',
-    marginBottom:'auto',
-    marginLeft:'auto',
-    marginRight:'auto',
-    //  width:'100%',
-    //    marginTop:'auto',
-    //    marginLeft:'auto',
-    //    marginRight:'auto',
-    //    marginBottom:'auto',
-
+    width: '100%',
+    height: '100%',
+    marginTop: 'auto',
+    marginBottom: 'auto',
+    marginLeft: 'auto',
+    marginRight: 'auto',
   },
-
-
 });
 
 let pvServerBASEURL;
-if(typeof process.env.REACT_APP_PyEpicsServerBASEURL==='undefined'){
-  pvServerBASEURL= "http://127.0.0.1";
+if (typeof process.env.REACT_APP_PyEpicsServerBASEURL === 'undefined') {
+  pvServerBASEURL = "http://127.0.0.1";
 }
-else{
-  pvServerBASEURL=process.env.REACT_APP_PyEpicsServerBASEURL;
+else {
+  pvServerBASEURL = process.env.REACT_APP_PyEpicsServerBASEURL;
 }
 
 let port;
-if(typeof process.env.REACT_APP_StyleguideServerPORT==='undefined'){
-  port= 6060;
+if (typeof process.env.REACT_APP_StyleguideServerPORT === 'undefined') {
+  port = 6060;
 }
-else{
-  port=process.env.REACT_APP_StyleguideServerPORT;
+else {
+  port = process.env.REACT_APP_StyleguideServerPORT;
 }
 
-let AutomationStudioStyleGuideBuildURL=pvServerBASEURL+":"+port;
+let AutomationStudioStyleGuideBuildURL = pvServerBASEURL + ":" + port;
 
 
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.logout=this.logout.bind(this);
-    this.state={redirectToLoginPage:false}
+    this.logout = this.logout.bind(this);
+    this.state = { redirectToLoginPage: false }
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('jwt');
 
   }
-  componentDidMount()
-  {
+
+  componentDidMount() {
     console.log('main mounted')
   }
   render() {
-
-
     const { classes } = this.props;
 
     return (
       <React.Fragment>
-        <RedirectToLogIn/>
+        <RedirectToLogIn />
         <Grid container direction="row" item justify="center" spacing={1} alignItems="center">
           <Grid item lg={4} sm={4} xs={2}>
-            <SideBar/>
+            <SideBar />
           </Grid>
 
-          <Grid  item lg={2} sm={4}  xs={8}>
+          <Grid item lg={2} sm={4} xs={8}>
 
-            <div style={{textAlign: 'center'}} className={classes.body1}>React Automation Studio V1.2.3</div>
+            <div style={{ textAlign: 'center' }} className={classes.body1}>React Automation Studio V1.2.3</div>
 
           </Grid>
           <Grid item lg={4} sm={4} xs={2}>
@@ -105,33 +84,20 @@ class Main extends Component {
           </Grid>
         </Grid>
         <Grid container direction="row" item justify="center" spacing={1} alignItems="center">
-          <Grid item lg={2} sm={4}  xs={8}>
+          <Grid item lg={2} sm={4} xs={8}>
             <Grid container direction="row" justify="center" spacing={3} alignItems="stretch">
               <Grid item xs={12}  >
-                  <Button className= {classes.button} component={Link} to="/Demos" color="primary" variant='contained'> Demos </Button>
-              </Grid>
-
-              <Grid item xs={12}  >
-                  <Button className= {classes.button} component={Link} to="/Staging" color="primary" variant='contained'>  Staging </Button>
+                <Button className={classes.button} component={Link} to="/MyPage" color="primary" variant='contained'>MyPage</Button>
               </Grid>
               <Grid item xs={12}  >
-              <Button className= {classes.button} target="_blank" href={AutomationStudioStyleGuideBuildURL} color="secondary" variant='contained'>  Help </Button>
+                <Button className={classes.button} target="_blank" href={AutomationStudioStyleGuideBuildURL} color="secondary" variant='contained'>  Help </Button>
               </Grid>
             </Grid>
-
           </Grid>
         </Grid>
+      </React.Fragment>
+    )
+  }
+}
 
-
-
-
-
-
-
-
-          </React.Fragment>
-          )
-          }
-          }
-
-          export default withRouter(withStyles(styles)(Main));
+export default withRouter(withStyles(styles)(Main));
